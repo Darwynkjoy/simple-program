c13328da19c9886f7d2686b855a14adef9feb3b8
darwynkjoy <darwynkjoy@gmail.com>
2024-09-07 16:20:54 +0530

    first programs

---
+++ numbers.dart
@@ -0,0 +1,12 @@
+void main(){
+int num1=17;
+int num2=20;
+double num3=31.43;
+num num4=30;
+num num5=100;
+print("num1 is $num1");
+print("num2 is $num2");
+print("num3 is $num3");
+print("num4 is $num4");
+print("num5 is $num5");
+}
---
+++ string.dart
@@ -0,0 +1,7 @@
+void main(){
+String firstName="darwyn";
+String lastName="k joy";
+String currentAddress="palakkal";
+print("firstname is $firstName and lastname is $lastName");
+print("current address is $currentAddress");
+}
